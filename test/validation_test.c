75ce89dadaacac264097977ff90f01768cc811a4
Свистунова Дарья Вадимовна <ri68s16@mwp02.csc.local>
2017-05-05 17:07:43 +0700

    Create test fot deposit_test.c

--- test/validation_test.c
+++ test/validation_test.c
@@ -1,14 +1,16 @@
 #include "../thirdparty/ctest.h"
 
-CTEST(suite, TEST_DAYS)
-{
-	const int days = 320;
-	
-	ASSERT_INTERVAL(1,365,days);
-} 
 
-CTEST(suite, TEST_MONEY)
+CTEST(percent, percent_days)
 {
-	const int money = 11000;
-	ASSERT_INTERVAL(10000, 999999, money);
+    const int days = 320;
+   
+    ASSERT_INTERVAL(1,365,days);
+}
+
+
+CTEST(percent, percent_moneys)
+{
+    const int money = 11000;
+    ASSERT_INTERVAL(10000, 999999, money);
 } 	
